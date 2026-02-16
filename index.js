54ed8d8e0ad1d075d266cee3db0c6b919efc5079
Ritesh <ritesh@example.com>
2026-02-16 20:32:33 +0530

    Merge remote main with local backend

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import connectDB from './config/db.js';
-import app from './src/app.js';
+import server from './server.js';
 
 if (process.env.NODE_ENV !== 'test') {
     connectDB();
 
     const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+    server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 }
